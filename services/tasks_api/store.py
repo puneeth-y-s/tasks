2251993bdfe3efab6058607f6003b27d8b406ef0
puneeth-y-s <yspuneeth1994@gmail.com>
2025-09-24 15:56:51 +0530

    Created cognito userpool and cognito userpool client

--- services/tasks_api/store.py
+++ services/tasks_api/store.py
@@ -9,11 +9,12 @@ from models import Task, TaskStatus
 
 class TaskStore:
 
-    def __init__(self, table_name):
+    def __init__(self, table_name, dynamodb_url=None):
         self.table_name = table_name
+        self.dynamodb_url = dynamodb_url
 
     def add(self, task):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         table.put_item(
             Item={
@@ -29,7 +30,7 @@ class TaskStore:
         )
 
     def get_by_id(self, task_id, owner):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         record = table.get_item(
             Key={
@@ -51,7 +52,7 @@ class TaskStore:
         return self._list_by_status(owner, TaskStatus.CLOSED)
 
     def _list_by_status(self, owner, status):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         last_key = None
         query_kwargs = {
